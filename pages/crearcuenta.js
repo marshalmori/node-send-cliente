bf684c52746312c5af2daf2ff2438e8f43655608
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-04-27 21:40:07 -0300

    Adicionar o Formik nos campos do formulário

--- pages/crearcuenta.js
+++ pages/crearcuenta.js
@@ -1,7 +1,19 @@
-import React from "react";
 import Layout from "@/components/Layout";
+import { useFormik } from "formik";
 
 const CrearCuenta = () => {
+  // Fomulario y validación con Formik y Yup
+  const formik = useFormik({
+    initialValues: {
+      nombre: "",
+      email: "",
+      password: "",
+    },
+    onSubmit: (valores) => {
+      console.log(valores);
+    },
+  });
+
   return (
     <Layout>
       <div className="md:w-4/5 xl:w-3/5 mx-auto mb-32">
@@ -11,8 +23,8 @@ const CrearCuenta = () => {
         <div className="flex justify-center mt-5">
           <div className="w-full max-w-lg">
             <form
-              action=""
               className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4"
+              onSubmit={formik.handleSubmit}
             >
               <div className="mb-4">
                 <label
@@ -26,6 +38,9 @@ const CrearCuenta = () => {
                   className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                   id="nombre"
                   placeholder="Nombre de Usuario"
+                  value={formik.values.nombre}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
                 />
               </div>
 
@@ -41,6 +56,9 @@ const CrearCuenta = () => {
                   className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                   id="email"
                   placeholder="Email de Usuario"
+                  value={formik.values.email}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
                 />
               </div>
 
@@ -56,6 +74,9 @@ const CrearCuenta = () => {
                   className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                   id="password"
                   placeholder="Password de Usuario"
+                  value={formik.values.password}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
                 />
               </div>
               <input
