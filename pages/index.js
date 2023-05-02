8120979f38ea4369b7e5f0264dc8856c1726467d
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-05-02 11:22:17 -0300

    Criar a disposição dos componentes da página principal

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import Layout from "@/components/Layout";
 import authContext from "@/context/auth/authContext";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +18,34 @@ export default function Index() {
 
   return (
     <Layout>
-      <h1 className="container">Index</h1>
+      <div className="md:w-4/5 xl:w-3/5 mx-auto mb-32">
+        <div className="lg:flex md:shadow-lg p-5 bg-white rounded-lg py-10">
+          <div className="md:flex-1 mb-3 mx-2 mt-16 lg:mt-0">
+            <p>Dropzone</p>
+          </div>
+
+          <div className="md:flex-1 mb-3 mx-2 mt-16 lg:mt-0">
+            <h2 className="text-4xl font-sans font-bold text-gray-800 my-4">
+              Compartir archivos de forma sencilla y privada
+            </h2>
+
+            <p className="text-lg leading-loose">
+              <span className="text-red-500 font-bold">ReactNodeSend</span> te
+              permite compartir archivos con cifrado de extremo a extremo y un
+              archivo que es eliminado después de ser descargado. Así que puedes
+              mantener lo que compartes en privado y asegurarte de que tus cosas
+              no permanezcan en línea para siempre.
+            </p>
+
+            <Link
+              href="/crearcuenta"
+              className="text-red-500 font-bold text-lg hover:text-red-700"
+            >
+              Crea una cuenta para mayores beneficios
+            </Link>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 }
