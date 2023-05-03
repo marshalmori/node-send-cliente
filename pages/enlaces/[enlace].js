c20c34782adae505d873289dfe4459f531702245
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-05-03 10:51:34 -0300

    Finalizar as funções getStaticProps e getStaticPaths

--- pages/enlaces/[enlace].js
+++ pages/enlaces/[enlace].js
@@ -1,11 +1,29 @@
 import Layout from "@/components/Layout";
 import clienteAxios from "@/config/axios";
 
-export async function getStaticProps() {}
+export async function getStaticProps() {
+  const resultado = await clienteAxios.get("/api/enlaces/HYT3aGiGA");
 
-export async function getStaticPaths() {}
+  return {
+    props: {
+      enlace: resultado.data,
+    },
+  };
+}
 
-export default () => {
+export async function getStaticPaths() {
+  const enlaces = await clienteAxios.get("/api/enlaces");
+
+  return {
+    paths: enlaces.data.enlaces.map((enlace) => ({
+      params: { enlace: enlace.url },
+    })),
+    fallback: false,
+  };
+}
+
+export default ({ enlace }) => {
+  console.log(enlace);
   return (
     <Layout>
       <h1>Desde [enlace].js</h1>
