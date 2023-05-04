bdf0fc3160fb07b2017ca2b9bf445db05cce9a5d
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-05-03 21:11:36 -0300

    Mostrar um formulário para criar uma senha de acesso ao arquivo enviado

--- components/Formulario.js
+++ components/Formulario.js
@@ -1,17 +1,40 @@
+import { useState } from "react";
+
 const Formulario = () => {
+  const [tienePassword, setTienePassword] = useState(false);
+
   return (
     <div className="w-full mt-20">
       <div>
-        <label className="text-lg text-gra-800">Eliminar tras:</label>
+        <label className="text-lg text-gray-800">Eliminar tras:</label>
         <select className="appearance-none w-full mt-2 bg-white border border-gray-400 text-black py-3 px-4 pr-8 rounded leading-none focus:outline-none focus:border-gray-500">
           <option value="" selected disabled>
-            -- Seleccione--
+            --- Seleccione ---
           </option>
           <option value="1">1 Descarga</option>
           <option value="5">5 Descargas</option>
           <option value="10">10 Descargas</option>
           <option value="20">20 Descargas</option>
         </select>
+      </div>
+
+      <div className="mt-5">
+        <div className="flex justify-between item-center">
+          <label className="text-lg text-gray-800 mr-2">
+            Proteger con Contrasenã:
+          </label>
+          <input
+            className="w-5 h-5"
+            type="checkbox"
+            onChange={() => setTienePassword(!tienePassword)}
+          />
+        </div>
+        {tienePassword ? (
+          <input
+            type="password"
+            className="appearance-none w-full mt-2 bg-white border border-gray-400 text-black py-3 px-4 pr-8 rounded leading-none focus:outline-none focus:border-gray-500"
+          />
+        ) : null}
       </div>
     </div>
   );
